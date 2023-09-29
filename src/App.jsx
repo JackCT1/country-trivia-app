56cb5d75b76b140269965e62d9daf35f3b073c23
JackCT1 <jackthomas933@gmail.com>
2023-09-29 17:51:22 +0100

    importing and displaying components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+import CountryList from "./components/list/CountryList";
+import CountryDisplay from "./components/display/CountryDisplay";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <CountryList />
+      <CountryDisplay />
+    </div>
+  );
 }
 
-export default App
+export default App;
