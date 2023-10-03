b884fcb97fb68e4546af64acc878f942bbda1097
JackCT1 <jackthomas933@gmail.com>
2023-10-03 23:16:36 +0100

    initialising selected country state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import CountryList from "./components/list/CountryList";
 import CountryDisplay from "./components/display/CountryDisplay";
+import { useState } from "react";
 
 function App() {
+  const [selectedCountry, setSelectedCountry] = useState("");
   return (
     <div className="App">
       <CountryList />
