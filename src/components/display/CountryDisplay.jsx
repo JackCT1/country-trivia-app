35889db186ce15ef953e4119efbac9dbefafc46c
JackCT1 <jackthomas933@gmail.com>
2023-10-03 21:34:54 +0100

    displaying country name buttons

--- src/components/display/CountryDisplay.jsx
+++ src/components/display/CountryDisplay.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function CountryDisplay() {
   const [countryData, setCountryData] = useState(undefined);
 
-  useEffect(async () => {
-    fetchData('spain')
-  }, [])
-
-  function fetchData(country) {
-    const apiResponse = await fetch(
-      `https://restcountries.com/v3.1/name/${country}`
-    )
-    const countryData = await apiResponse.json()
-    setCountryData(countryData)
-  }
+  useEffect(() => {
+    const getCountries = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const countryData = await response.json();
+        setCountryData(countryData);
+      } catch (error) {
+        console.log("Error:" + error);
+      }
+    };
+    getCountries();
+  }, []);
 
   if (!countryData) {
     return <div>Loading...</div>;
   }
 
-  return <div>{JSON.stringify(countryData)}</div>;
+  return (
+    <div className="content">
+      <h1>List of Countries</h1>
+      <div>
+        {countryData.map((country) => {
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <button>
+              <h2>{country.name.common}</h2>
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
